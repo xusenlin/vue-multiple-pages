3c23cb58f9c7e25049077e87a65d16534b33e83d
737657495@qq.com <1468756621306qq>
2018-12-08 17:36:24 +0800

    修改注释

--- src/utils/setRem.js
+++ src/utils/setRem.js
@@ -1,16 +1,16 @@
 
+
 // 基准大小
 const baseSize = 100
-// 设置 rem 函数
+
 function setRem() {
     // 当前页面宽度相对于 375 宽的缩放比例，可根据自己需要修改。
     const scale = document.documentElement.clientWidth / 375
-    // 设置页面根节点字体大小
     document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px'
 }
 // 初始化
 setRem()
-// 改变窗口大小时重新设置 rem
+// 改变窗口大小时重新设置 fontSize
 window.onresize = function() {
     setRem()
 }
