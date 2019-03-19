7a7e58ec4705f0a21034e27e6b34ec4596805f91
737657495@qq.com <1468756621306qq>
2019-03-19 11:46:59 +0800

    添加请求提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,8 +10,39 @@ const service = Axios.create({
     timeout: Config.timeout
 })
 
+
+service.interceptors.request.use(
+    config => {
+
+        if(!config.closeLoading){
+            //加载提示
+            window.loadingInstance = Toast.loading({
+                mask: true,
+                message: '加载中...'
+            });
+        }
+
+        let noParameters = config.url.indexOf('?')  == -1;
+        //config.headers['X-Token'] = getToken() //
+        config.url = noParameters ? config.url+'?access_token=' + getToken(): config.url+'&access_token='+ getToken();
+
+        return config
+    },
+    error => {
+        Promise.reject(error)
+    }
+)
+
 service.interceptors.response.use(
     response => {
+        if(!response.config.closeLoading){
+            setTimeout(_=>{
+                if(window.loadingInstance){
+                    window.loadingInstance.clear();
+                }
+            },400);
+        }
+
         const res = response
         if (res.status !== 200) {
             Toast('数据返回出错');
@@ -25,6 +56,12 @@ service.interceptors.response.use(
         }
     },
     error => {
+
+        setTimeout(_=>{
+            if(window.loadingInstance){
+                window.loadingInstance.clear();
+            }
+        },300);
         Toast("请求未响应");
         return Promise.reject(error)
     }
