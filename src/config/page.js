fd6e254cd2ac67b820a279113e2fc24ddaedcb74
五木老祖 <wumulaozu@gmail.com>
2019-08-10 11:29:16 +0800

    优化Url参数验证报错

--- src/config/page.js
+++ src/config/page.js
@@ -2,7 +2,8 @@ module.exports = {
     demo:{
         title:'演示',
         requiredParams:{//必填参数
-            userId:'url必须携带用户Id'
+            userId:'url必须携带用户Id',
+            ll:'asasdsadsa'
         },
         optionalParams:{//可选参数
         },
--- src/utils/validateAndInitUrlParams.js
+++ src/utils/validateAndInitUrlParams.js
@@ -1,52 +1,24 @@
 import {currentUrlToParams} from './app.js'
 import allPageConfig from '@/config/page.js'
 
-
-
-
 const currentPageParams = currentUrlToParams();
 let pageConfig = allPageConfig[window.PAGE_PATH]; //PAGE_PATH在public中定义。
 
 export function validateAndInitUrlParams() {
     window.URL_PARAMS = currentPageParams;
-    let noRequiredParams = false;
-    let errorMsg = '';
+    let errorMsg = [];
     if(pageConfig.requiredParams && Object.keys(pageConfig.requiredParams).length != 0){
         let requiredParams = pageConfig.requiredParams;
-        errorMsg = 'Url缺少参数:';
         for (let key in requiredParams){
             if(!URL_PARAMS.hasOwnProperty(key)){
-                errorMsg += `[${key}(${requiredParams[key]})]`;
-                noRequiredParams = true
+                errorMsg.push({name:key,desc:requiredParams[key]});
             }
         }
     }
-    // if(pageConfig.localStorage && Object.keys(pageConfig.localStorage).length != 0 && !noRequiredParams){
-    //     let requiredParams = pageConfig.localStorage;
-    //     errorMsg = 'localStorage缺少数据:';
-    //
-    //     for (let key in requiredParams){
-    //         if(!localStorage.getItem(key)){
-    //             errorMsg += `[${key}${requiredParams[key]}]`;
-    //             noRequiredParams = true
-    //         }
-    //     }
-    // }
-    // if(pageConfig.sessionStorage && Object.keys(pageConfig.sessionStorage).length != 0 && !noRequiredParams){
-    //     let requiredParams = pageConfig.sessionStorage;
-    //     errorMsg = 'sessionStorage缺少数据:';
-    //
-    //     for (let key in requiredParams){
-    //         if(!sessionStorage.getItem(key)){
-    //             errorMsg += `[${key}${requiredParams[key]}]`;
-    //             noRequiredParams = true
-    //         }
-    //     }
-    // }
-    if(noRequiredParams){
-        alert(errorMsg);
-        throw new Error(errorMsg);
-        return false
+    if(errorMsg.length != 0){
+        console.table(errorMsg);
+        document.body.innerHTML = `<div style="width: 100%;height: 100vh;display: flex;justify-content: center;align-items: center">页面路径出错，请尝试重新打开。</div>`
+        throw new Error('Url缺少必填参数！');
     }
 
     return true
